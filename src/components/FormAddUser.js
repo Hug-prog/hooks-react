13603dc441b370da8788aa20bf959008d8bdefbe
Hug-prog <80644752+Hug-prog@users.noreply.github.com>
2022-11-14 14:59:51 +0100

    add form

---
+++ src/components/FormAddUser.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+const FormAddUser = ({ list, handleChangeState }) => {
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    e.target.reset();
+    handleChangeState({ id: list.length + 1, name: name, userName: userName });
+  };
+
+  return (
+    <div className="add_user">
+      <h3>add new user</h3>
+      <form onSubmit={e => handleSubmit(e)}>
+        <div>
+          <div>
+            {" "}
+            <input
+              placeholder="Name"
+              type="text"
+              onChange={e => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <input
+              placeholder="UserName"
+              type="text"
+              onChange={e => {
+                setUserName(e.target.value);
+              }}
+            />
+          </div>
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default FormAddUser;
